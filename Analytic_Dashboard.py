65a5e299cf5f828a7feb842eb70fba875e822faf
Bouhlal Yassine <bouhlalyas@gmail.com>
2023-09-19 12:03:27 +0100

    inital commit

---
+++ Analytic_Dashboard.py
@@ -0,0 +1,254 @@
+import tempfile
+import time
+import streamlit as st
+from settings import *
+from streamlit_lottie import st_lottie
+import streamlit as st
+import streamlit_authenticator as stauth
+from streamlit_option_menu import option_menu
+import yaml
+from yaml.loader import SafeLoader
+
+# streamlit run Analytic_Dashboard.py
+
+st.set_page_config(page_title=TITLE,
+    page_icon=PAGE_ICON,
+    layout="wide")
+
+
+with open(config_Dash) as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+with open(css_file) as f: # Load the CSS file
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+
+st.markdown("<h2 style=\
+    'text-align : center';\
+    font-weight : bold ;\
+    font-family : Arial;>\
+    Analytic Dashboard</h2>", unsafe_allow_html=True)
+
+st.markdown("""---""")
+
+with st.sidebar :
+    clickable_img_logo = get_img_with_href(pp_logo_portfolio, 'https://ybouhlal.streamlit.app/', 70, "blank")
+    st.markdown(clickable_img_logo, unsafe_allow_html=True)
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    clickable_img = get_img_with_href(linkpic_code, 'https://docs.streamlit.io', 170, "blank")
+    st.markdown(clickable_img, unsafe_allow_html=True)
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    nav_menu = option_menu(menu_title=None, options=['Home', 'Example APP'], 
+        default_index=0, orientation="vertical",
+        icons=["house", "app"],
+        styles={
+            "container": {"padding": "0!important"},
+            "nav-link": {"font-size": "14px", "text-align": "left", "margin":"2px", "--hover-color": "#805E83"}
+        })
+
+
+
+if nav_menu == 'Home':
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    colpi1, colpi2 = st.columns([75, 25], gap="small")
+    with colpi1:
+        st.info("Analytic Dashboard, is a simple example of a data analysis webapp that allows you to :\
+            \n ● Automate data processing\
+            \n ● Choose the data to display using filters\
+            \n ● Get interactive charts\
+            \n ● View and download (custom) data tables\
+            \n ● Regulate access through a user interface\
+            \n ● Access on both PC and smartphone\
+            \n\n ► In the example that I am providing, I am working with a random database (Excel),\
+                but this is possible with any type of database\
+            \n ► In order to access the app please click on [Example APP] on the left side menu")
+    
+    with colpi2:
+        st.markdown("<br>", unsafe_allow_html=True)
+        lottie_dashb = load_lottiefile(lottie_dashb)
+        st_lottie(
+            lottie_dashb,
+            speed=1,
+            reverse=False,
+            loop=True,
+            quality="high", # medium ; high ; low
+            height=250)
+        
+    st.markdown("<br>", unsafe_allow_html=True)
+    esp_1, col_vid_tuto, esp_2 = st.columns([space, tuto_space, space], gap="small")
+    with col_vid_tuto :
+        with open(tuto_analyticdash, "rb") as tuto_file:
+            tuto_analyticdash_byte = tuto_file.read()
+        st.video(tuto_analyticdash_byte)
+
+if nav_menu == 'Example APP':
+    
+    infos = st.info("Trial credentials : ID = invite | Password = invite")
+
+    name, authentication_status, username = authenticator.login("Login", "main")
+
+    if authentication_status == False:
+        st.error("Username/password is incorrect")
+
+    if authentication_status == None:
+        st.warning("Please enter your username and password")
+
+    if authentication_status:
+        # Original DF
+        main_df = get_data_from_dataset(Excel_BdD)
+        infos.empty()
+    
+        options_dash = st.sidebar.radio(label="Sections :", options=['Overview', 'View/Extract Data'],
+            index=0,  label_visibility="hidden")
+
+        if options_dash == 'Overview':
+            col0F_f, col1F_f, col2F_f = st.columns(3)
+
+            with col0F_f:           
+                options_city = ["ALL"] + list(main_df["City"].unique())
+                ville = st.multiselect(label="📌 City :",
+                    options=options_city,
+                    default="ALL", label_visibility="visible") # "hidden"
+                if "ALL" in ville:
+                    df_filtr  = main_df
+                else:
+                    df_filtr = main_df[main_df["City"].isin(ville)]
+    
+            with col1F_f:
+                options_sex = ["ALL"] + list(df_filtr["Gender"].unique())
+                sex = st.multiselect(label="📌 Gender :",
+                    options=options_sex,
+                    default="ALL", label_visibility="visible") # "hidden"
+                if "ALL" in sex:
+                    df_filtr  = df_filtr
+                else:
+                    df_filtr = df_filtr[df_filtr["Gender"].isin(sex)]
+
+            with col2F_f:
+                options_P = ["ALL"] + list(sorted(df_filtr["Type_produit"].unique()))
+                type_p = st.multiselect(label="📌 Product type :",
+                    options=options_P,
+                    default="ALL", label_visibility="visible") # "hidden"
+                if "ALL" in type_p:
+                    df_filtr  = df_filtr
+                else:
+                    df_filtr = df_filtr[df_filtr["Type_produit"].isin(type_p)]
+
+            st.markdown("""---""")
+        
+            # Main KPI's
+            tot_Achats = round(total_kpi(df_filtr)[0], 1)
+            tot_ventes = round(total_kpi(df_filtr)[1], 1)
+            tot_taxes = round(total_kpi(df_filtr)[2], 1)
+            tot_profit_net = round(total_kpi(df_filtr)[3], 1)
+
+            col0, col1, col2, col3 = st.columns(4)
+            with col0:
+                st.subheader("Total Purchases")
+                st.subheader(f"{tot_Achats:,} $".format(tot_Achats).replace(',', ' '))
+                # st.subheader(f"T {total_Arr:,}")
+            with col1:
+                st.subheader("Total Sales")
+                st.subheader(f"{tot_ventes:,} $".format(tot_ventes).replace(',', ' '))
+            with col2:
+                st.subheader("Total Taxes")
+                st.subheader(f"{tot_taxes:,} $".format(tot_taxes).replace(',', ' '))
+            with col3:
+                st.subheader("Net Profit")
+                st.subheader(f"{tot_profit_net:,} $".format(tot_profit_net).replace(',', ' '))
+
+            st.markdown("""---""")
+
+            col_1_0, col_1_1 = st.columns(2)
+            with col_1_0:
+                st.subheader("Sales by Gender")
+                st.plotly_chart(piechart_sales_gender(df_filtr), use_container_width=True) 
+
+            with col_1_1:
+                st.subheader("Sales by city")
+                st.plotly_chart(piechart_sales_city(df_filtr), use_container_width=True)
+
+            st.markdown("""---""")
+
+            st.subheader("Sales volume by payment method")
+            st.plotly_chart(bar_chart_pay_type(df_filtr), use_container_width=True)
+
+            st.markdown("""---""")
+
+            st.subheader("Evolution by week")
+            st.plotly_chart(get_data_sem_chart(df_filtr), use_container_width=True)
+
+
+        if options_dash == 'View/Extract Data':
+
+            col0Fa_f, col1Fa_f, col2Fa_f = st.columns(3)
+
+            with col0Fa_f:           
+                options_city_a = ["ALL"] + list(main_df["City"].unique())
+                ville_a = st.multiselect(label="📌 City :",
+                    options=options_city_a,
+                    default="ALL", label_visibility="visible") # "hidden"
+                if "ALL" in ville_a:
+                    df_filtr_a  = main_df
+                else:
+                    df_filtr_a = main_df[main_df["City"].isin(ville_a)]
+    
+            with col1Fa_f:
+                options_sex_a = ["ALL"] + list(df_filtr_a["Gender"].unique())
+                sex_a = st.multiselect(label="📌 Gender :",
+                    options=options_sex_a,
+                    default="ALL", label_visibility="visible") # "hidden"
+                if "ALL" in sex_a:
+                    df_filtr_a  = df_filtr_a
+                else:
+                    df_filtr_a = df_filtr_a[df_filtr_a["Gender"].isin(sex_a)]
+
+            with col2Fa_f:
+                options_P_a = ["ALL"] + list(sorted(df_filtr_a["Type_produit"].unique()))
+                type_p_a = st.multiselect(label="📌 Product type :",
+                    options=options_P_a,
+                    default="ALL", label_visibility="visible") # "hidden"
+                if "ALL" in type_p_a:
+                    df_filtr_a  = df_filtr_a
+                else:
+                    df_filtr_a = df_filtr_a[df_filtr_a["Type_produit"].isin(type_p_a)]
+
+            st.markdown("""---""")
+
+            data_extract_df = get_showndf(df_filtr_a)
+            
+            temp = tempfile.NamedTemporaryFile(delete=True)
+            temp_filename = temp.name + f'.xlsx'
+            data_extract_df.to_excel(temp_filename, sheet_name='Data', index=False)
+
+            last_file = curstom_excel_df(temp_filename)
+
+            with open(last_file, "rb") as f:
+                binary_data = f.read()
+
+            timestr = time.strftime("%d-%H%M%S")
+            excel_namefile  = f"Data_{timestr}.xlsx"
+
+            st.download_button(
+                label="Download Data to Excel",
+                data=binary_data,
+                file_name=excel_namefile)
+            st.dataframe(data_extract_df.style.format(na_rep='No Data', precision=1), use_container_width=True)
+
+
+        # Auth Logout
+        st.sidebar.markdown("<br>", unsafe_allow_html=True)
+        st.sidebar.divider()
+        authenticator.logout("Log Out", "sidebar")
---
+++ settings.py
@@ -0,0 +1,275 @@
+import json
+from pathlib import Path
+import requests
+import plotly.express as px
+import openpyxl as oxl
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import plotly.graph_objects as go
+from openpyxl.styles import Font, Color, Fill, PatternFill, Border, Side, numbers, Alignment
+from openpyxl.utils import get_column_letter
+import os
+import base64
+
+TITLE = "Analytic Dashbord"
+PAGE_ICON ="ico_potfolio.ico"
+
+
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+
+
+# Credentials
+config_Dash = current_dir / "hashed_pw.yaml"
+
+
+# CSS
+css_file = current_dir / "main.css"
+
+pp_logo_portfolio = current_dir / "files" /  "logo_portfolio.png"
+linkpic_code = current_dir / "files" /  "code.png"
+
+# Tutos :
+# size :
+space = 15
+tuto_space = 70
+tuto_analyticdash_p = current_dir / "files" / "tuto_analyticdash.mp4"
+tuto_analyticdash = str(tuto_analyticdash_p)
+
+# lotties :
+lottie_dashb = current_dir / "files" / "dashb.json"
+
+def load_lottiefile(filepath : str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# Clickable img
+@st.cache_data
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+@st.cache_data
+def get_img_with_href(local_img_path, target_url, width, loc):
+    img_format = os.path.splitext(local_img_path)[-1].replace('.', '')
+    bin_str = get_base64_of_bin_file(local_img_path)
+    html_code = f'''
+        <a href="{target_url}" target="_{loc}" style="display: flex; justify-content: center; align-items: center;">
+            <img src="data:image/{img_format};base64,{bin_str}" width="{width}" class="img-hover-effect">
+        </a>'''
+    return html_code
+
+Excel_BdD = current_dir / "files" / "EXP_excel_data.xlsx"
+
+@st.cache_data
+def get_data_from_dataset(Excel_BdD):
+    wb = oxl.load_workbook(Excel_BdD)
+    wb_sheets = wb.sheetnames
+    df = pd.read_excel(io=Excel_BdD, sheet_name=wb_sheets[0], header=0)
+    return df
+
+
+def total_kpi(df):
+    tot_Achats = (df['Prix_Achat']*df['Qté_vendue']).sum()
+    tot_ventes = df['Prix_vente_Total'].sum()
+    tot_taxes = df['Taxe_5%'].sum()
+    tot_profit_net= df['Profit_net'].sum()
+
+    return [tot_Achats,tot_ventes,tot_taxes,tot_profit_net]
+
+
+# graph volume des ventes / sexe 
+def piechart_sales_gender(df):
+    df_grouped_arr = df.groupby(by=['Gender']).sum()[['Prix_vente_Total']]
+
+    pie_chart = px.pie(df_grouped_arr,
+        values='Prix_vente_Total',
+        names=df_grouped_arr.index,
+        color=df_grouped_arr.index,
+        color_discrete_map = {
+            'Female': '#D67FD4',
+            'Male': '#4572B3'})
+
+    # Customize the pie chart
+    pie_chart.update_traces(
+        textposition='auto', #'inside', 'outside' 
+        textinfo='percent+label')
+
+    pie_chart.update_layout(
+        font=dict(size=12, family='Arial Black'),
+        legend=dict(
+            font=dict(size=12, family='Arial Black'),
+            x=0.1,
+            y=-0.1,
+            orientation='h'),
+        width = 400,
+        height = 400)
+
+    return pie_chart
+
+
+# graph volume des ventes / ville 
+def piechart_sales_city(df):
+    df_grouped_arr = df.groupby(by=['City']).sum()[['Prix_vente_Total']]
+
+    pie_chart = px.pie(df_grouped_arr,
+        values='Prix_vente_Total',
+        names=df_grouped_arr.index,
+        color=df_grouped_arr.index,
+        color_discrete_map = {
+            'Casablanca': '#D1E04F',
+            'Rabat': '#53E04F',
+            'Marrakech': '#9B3445'})
+
+    # Customize the pie chart
+    pie_chart.update_traces(
+        textposition='auto', #'inside', 'outside' 
+        textinfo='percent+label')
+
+    pie_chart.update_layout(
+        font=dict(size=12, family='Arial Black'),
+        legend=dict(
+            font=dict(size=12, family='Arial Black'),
+            x=0.25,
+            y=-0.1,
+            orientation='h'),
+        width = 400,
+        height = 400)
+
+    return pie_chart
+
+
+# graph volume des ventes / pay_type 
+@st.cache_data
+def bar_chart_pay_type(df):
+    
+    df_grouped = df.groupby(by=['Payment_type']).sum()[['Prix_vente_Total']]
+    # df1 = df.pivot_table(index="Producteur", columns="Station", values="Brix", aggfunc="mean") 
+    color_map = {
+        'Cash': 'green',
+        'Credit card': '#94362D',
+        'Phone wallet': '#2D5C94'}
+
+    bar_chart = px.bar(df_grouped, 
+        x=df_grouped.index, 
+        y="Prix_vente_Total", # df_grouped.values si +ieurs
+        color=df_grouped.index,
+        color_discrete_map=color_map, )
+
+    bar_chart.update_layout(
+        xaxis_title=None,  # Set the x-axis title
+        yaxis_title="Sales",  # Set the y-axis title
+        font=dict(size=12, family='Arial Black'),
+        legend=dict(title=None,x=0.1, y=1.1, orientation='h'),
+        width = 450,
+        height = 400,
+        bargap=0.6)
+    return bar_chart
+
+
+# Evolution 
+def get_data_sem_chart(df):
+
+    df_sem = df.groupby(["Semaine"], as_index=False).agg({
+    'Prix_vente_Total' : 'mean',
+    'Profit_net': 'mean',
+    'Qté_vendue':'mean'
+    }).round(2)
+
+    # Create the figure 
+    trace1_0 = go.Scatter(x=df_sem['Semaine'], y=df_sem['Prix_vente_Total'], name="Total Sales",
+        line=dict(width=2, color='#B4339D', dash='dash'), marker=dict(size=7, color='#B4339D'))
+
+    trace1_1 = go.Scatter(x=df_sem['Semaine'], y=df_sem['Profit_net'], name="Net Profit",
+        line=dict(width=2, color='#4088CD', dash='dash'), marker=dict(size=7, color='#4088CD'))
+
+    trace2_1 = go.Scatter(x=df_sem['Semaine'], y=df_sem['Qté_vendue'], yaxis='y2',name="Quantity sold (Nbr)",
+        line=dict(width=3, color='#7638C6'), marker=dict(size=10, color='#7638C6'))
+
+    # Create a figure and add the traces to it
+    fig = go.Figure()
+    fig.add_traces([trace1_0, trace1_1, trace2_1])
+
+    # Update the layout to include two y-axes
+    fig.update_layout(yaxis=dict(title="Total Sales"),
+        yaxis2=dict(title="Net Profit", overlaying='y', side='right'),
+        xaxis=dict(tickvals=df_sem['Semaine'], ticktext=df_sem['Semaine']), # Affcher toutes les valeurs
+        font=dict(size=12, family='Arial Black'),
+        legend=dict(orientation="h", x=0.1, y=1.1, font=dict(size=12,family='Arial Black')))
+
+    return fig
+
+
+# Extract Data
+def get_showndf(df):
+    ladt_df = df.loc[:,['Semaine', 'City', "Gender", 'Type_produit', 'Prix_Vente', 'Qté_vendue',
+        "Prix_vente_Total", 'Taxe_5%', "Profit_net", "Payment_type"]]
+
+    ladt_df.sort_values(["Semaine", 'Prix_vente_Total'], ascending=[True,False], inplace=True)
+    ladt_df.reset_index(drop=True, inplace=True)
+
+    return ladt_df
+
+
+# Curstom Excel file
+def curstom_excel_df(excel_file):
+    wb = oxl.load_workbook(excel_file)
+
+    # Backgrounds
+    Gris_fonce = PatternFill(patternType='solid', fgColor="0A0A0A") # Gris foncé
+    Marron_Clair = PatternFill(patternType='solid', fgColor="C4BD97") # Marron clair
+
+    # Borders
+    side1 = Side(border_style='thin', color="000000")
+    border1 = Border(top=side1 , bottom=side1, right=side1, left=side1)
+
+    # Fontstyles
+    font_style = Font(name= 'Calibri', size = 11, color= "000000", bold = True)
+
+    # Alignements
+    alignment_left = Alignment(horizontal="left",vertical="center")
+
+    """-----------------------------Mise en forme Feuilles -----------------------------""" 
+    ws = wb[wb.sheetnames[0]]
+    max_c = ws.max_column # le max de cols (exploités) dans la feuille
+    max_r = ws.max_row # le max de lignes (exploités) dans la feuille
+
+    cell_fil1 = get_column_letter(max_c)
+    ws.auto_filter.ref = f'A1:{cell_fil1}1' # Add a filter
+    
+    # Mise en forme cadre du tableau en gris :
+    for row in ws.iter_rows(min_row=1, min_col=1, max_row=max_r+10, max_col = max_c+10):
+        for cell in row:
+            cell.fill=Gris_fonce
+
+    # Mise en forme en-tete :
+    for cell in ws[1]:
+        cell.font=font_style
+
+    # Hauteur de la 1ere ligne
+    ws.row_dimensions[1].height = 30
+
+    for row in ws.iter_rows(min_row=1, max_row=max_r, min_col=1, max_col = max_c):
+        for cell in row:
+            cell.fill=Marron_Clair
+            cell.border=border1
+            cell.alignment = alignment_left
+
+
+    # modifier la largeur des colonnes :
+    # cf : https://stackoverflow.com/questions/47204711/openpyxl-change-width-of-n-columns
+    for index_col in range(1, max_c+1): # (inclu, exclu)
+        i = get_column_letter(index_col)
+        ws.column_dimensions[i].width = 13
+
+    wb.save(excel_file)
+    
+    return excel_file
